77491ee0a85f06dc046087efee46dde7dfa1aa54
Renish1234 <46221806+Renish1234@users.noreply.github.com>
2019-08-28 18:07:07 +0545

    part 7

--- polls/admin.py
+++ polls/admin.py
@@ -1,26 +1,8 @@
 from django.contrib import admin
+from .models import Question
+from .models import Choice
 
-from .models import Choice, Question
+admin.site.register(Question)
+admin.site.register(Choice)
 
-
-class ChoiceInline(admin.StackedInline):
-    model = Choice
-    extra = 3
-
-class QuestionAdmin(admin.ModelAdmin):
-    fieldsets = [
-        (None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
-    ]
-    inlines = [ChoiceInline]
-
-admin.site.register(Question, QuestionAdmin)
-
-
-class QuestionAdmin(admin.ModelAdmin):
-    # ...
-    list_display = ('question_text', 'pub_date')
-
-class QuestionAdmin(admin.ModelAdmin):
-    # ...
-    list_display = ('question_text', 'pub_date', 'was_published_recently')
+# Register your models here.
--- polls/models.py
+++ polls/models.py
@@ -3,9 +3,6 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
-def was_published_recently(self):
-    now = timezone.now()
-    return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Question(models.Model):
